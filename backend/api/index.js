e92a01425da2cce529a9b9f8d0c7a5aa92cb43f6
David Akinloye <69762085+David98843@users.noreply.github.com>
2023-06-07 15:52:00 +0100

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,6 +5,11 @@ require('dotenv').config()
 require('./middleware')(app)
 
 app.use('/', require('./routes'))
+app.get('/', (req, res) => {
+  res.send('Hello Express')  
+})
 
 const PORT = process.env.PORT || 30
 app.listen(PORT, console.log(`App started on PORT ${PORT}`))
+
+module.exports = app
